bf5cda554217668bf327f17f6d6caafa9a0111f0
raditya24ti <raditya24ti@mahasiswa.pcr.ac.id>
2026-04-03 17:33:08 +0700

    p3 part1

---
+++ src/pertemuan-2/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import HelloWorld from "./HelloWorld";
+
+createRoot(document.getElementById("root"))
+    .render(
+        <div>
+            <HelloWorld/>
+        </div>
+    );
+    
