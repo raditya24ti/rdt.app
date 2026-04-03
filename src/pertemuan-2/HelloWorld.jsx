f70b45d7c638527d7ce17a6f69c425ced5b4d2cd
raditya24ti <raditya24ti@mahasiswa.pcr.ac.id>
2026-04-03 18:55:50 +0700

    pertemuan 3

--- src/pertemuan-2/HelloWorld.jsx
+++ src/pertemuan-2/HelloWorld.jsx
@@ -1,8 +1,51 @@
 export default function HelloWorld(){
+    const propsUserCard = {
+        nama: "alisa",
+        nim: "2453526",
+        tanggal: "2025-02-02",
+    };
     return (
         <div>
             <h1>Hello World</h1>
             <p>Selamat Belajar ReactJs</p>
+            <GreetingBinjai/>
+            <QuoteText/>
+            <UserCard
+            nama="Radit"
+            nim="2455301155"
+            tanggal={new Date().toLocaleDateString()}
+            />
+            <UserCard{...propsUserCard}/>
+            <img src="/public/img/pcr.jpg" alt="Logo" />
         </div>
     );
+}
+
+ function GreetingBinjai(){
+    return (
+        <small>Salam Dari Binjay</small>
+    )
+ }
+
+function QuoteText() {
+    const text = "Mulutmu Harimaumu";
+    const text2 = "Aku ingin jadi macan";
+    return (
+        <div>
+            <hr/>
+            <p>{text.toLowerCase()}</p>
+            <p>{text2.toUpperCase()}</p>
+        </div>
+    )
+}
+
+function UserCard(props) {
+  return (
+    <div>
+      <hr />
+      <h3>Nama: {props.nama}</h3>
+      <p>NIM: {props.nim}</p>
+      <p>Tanggal: {props.tanggal}</p>
+    </div>
+  );
 }
--- src/pertemuan-2/main.jsx
+++ src/pertemuan-2/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from "react-dom/client";
 import HelloWorld from "./HelloWorld";
+import Container from "./Container";
+import './custom.css';
 
 createRoot(document.getElementById("root"))
-    .render(
-        <div>
-            <HelloWorld/>
-        </div>
-    );
-    
+  .render(
+    <div>
+      <Container>
+        <HelloWorld />
+      </Container>
+    </div>
+  )
+  
