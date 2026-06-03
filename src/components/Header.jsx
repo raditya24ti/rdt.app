499ecd61dff649667fb2dd2e98eb6d0dc2f28d0d
ADENIAFARIDHOTUL18 <raditya24ti@mahasiswa.pcr.ac.id>
2026-06-03 09:28:02 +0700

    Pertemuan12baru

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
                 {/* Profile Section */}
                 <div id="profile-container" className="flex items-center space-x-4 border-l pl-4 border-gray-300">
                     <span id="profile-text">
-                        Hello, <b>Regalo</b>
+                        Hai, <b>Regalo</b>
                     </span>
                     <img
                         id="profile-avatar"
